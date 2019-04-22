25328d939e70a6d9c127540b308bd43f6c815c40
IshantL <ishantlambhate@gmail.com>
2019-04-22 03:42:30 +0000

    removed typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'recat-redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
