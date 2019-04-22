a3188c8733ed074ab416a803785c41a4d90fe845
IshantL <ishantlambhate@gmail.com>
2019-04-22 03:41:22 +0000

    Added the redux and redux thunk, redux-devtools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'recat-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import { BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './rootReducer';
+
+const store = createStore(rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
 <BrowserRouter>
+<Provider store={store}>
 <App />
+</Provider>
 </BrowserRouter>
 , document.getElementById('root'));
 
