65de776925bb3e44382e6107c6a5aaf7c4405223
IshantL <ishantlambhate@gmail.com>
2019-04-22 12:53:24 +0000

    adding actions and dispatch functions

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,11 @@
 import React,{Component} from 'react';
 import LoginForm from '../forms/LoginForm.js'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../../actions/auth';
 
 class LoginPage extends Component{
-    submit = data =>{
-        console.log(data);
-    };
+    submit = data => this.props.login(data).then(()=> this.props.history.push("/"));
 
     render(){
         return(
@@ -16,4 +17,11 @@ class LoginPage extends Component{
     }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    login: PropTypes.func.isRequired
+};
+
+export default connect(null,{login})(LoginPage);
